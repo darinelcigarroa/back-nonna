7da84ca4697b8aeb80090b3fb854ce649962f425
Darinel <darinelcigarroa97@gmail.com>
2025-04-02 21:22:31 +0000

    :ambulance: UserSedeer original

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,7 +14,7 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory()->count(5)->create();
+        User::factory()->count(5)->create();
 
         $userSuperAdmin = User::updateOrCreate(
             ['email' => 'admin@admin.com'],
