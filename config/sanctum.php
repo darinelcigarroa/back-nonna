beb65246bb25d82e9dba459b58e08dc37bba4134
Darinel <darinelcigarroa97@gmail.com>
2025-04-08 01:13:43 +0000

    stateful

--- config/sanctum.php
+++ config/sanctum.php
@@ -14,12 +14,8 @@ return [
     | and production domains which access your API via a frontend SPA.
     |
     */
+'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'front-nonna-production.up.railway.app,back-nonna-production.up.railway.app')),
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort()
-    ))),
 
     /*
     |--------------------------------------------------------------------------
