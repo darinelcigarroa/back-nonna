d1365294b2b9a7dfb61c8f0c86f96337efb114f1
Darinel <darinelcigarroa97@gmail.com>
2025-04-10 23:06:28 +0000

    log facede

--- app/Events/TestEvent.php
+++ app/Events/TestEvent.php
@@ -1,12 +1,13 @@
 <?php
 namespace App\Events;
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
-use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
 class TestEvent implements ShouldBroadcast
 {
